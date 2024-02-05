f8fe6bb87172f33b24e978c9acc21dd10b6b6696
karinEkenberg <zirkon6@hotmail.com>
2024-02-05 20:22:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GenericListExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruitList = new List<string>();
+
+            Console.WriteLine("How many fruits do you want to add to the list?");
+            int amount = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < amount ; i++)
+            {
+                Console.WriteLine($"Fruit number {i+1}:");
+                string fruits = Console.ReadLine()!;
+                fruitList.Add(fruits);
+            }
+            Console.Clear();
+            Console.WriteLine("The fruits added are:");
+            PrintList(fruitList);
+        }
+
+        static void PrintList<T>(List<T> fruitList)
+        {
+            foreach (var item in fruitList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
